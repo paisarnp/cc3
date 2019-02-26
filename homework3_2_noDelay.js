d485dbd821a0247f069df5b4a0d486ac2667e14b
Ake <paisarnp@hotmail.com>
2019-02-26 22:50:53 +0700

    rename files

--- homework3_2_noDelay.js
+++ homework3_2_noDelay.js
@@ -1,20 +1,18 @@
 "use strict";
-const peopleSalary = [
+// const peopleSalary = [
+let peopleSalary = [
     { "id": "1001", "firstname": "Luke", "lastname": "Skywalker", "company": "Walt Disney", "salary": "40000" },
     { "id": "1002", "firstname": "Tony", "lastname": "Stark", "company": "Marvel", "salary": "1000000" },
     { "id": "1003", "firstname": "Somchai", "lastname": "Jaidee", "company": "Love2work", "salary": "20000" },
     { "id": "1004", "firstname": "Monkey D", "lastname": "Luffee", "company": "One Piece", "salary": "9000000" }
 ]
 console.log("Before peopleSalary =\n", peopleSalary);
-setTimeout(function() {
-    for (let i = 0; i < peopleSalary.length; i++)
+
+for (let i = 0; i < peopleSalary.length; i++)
     if (peopleSalary[i].company) 
         delete peopleSalary[i].company;
 
-    console.log("\nAfter peopleSalary =\n", peopleSalary)
-}, 5000);
-
-
+console.log("\nAfter peopleSalary =\n", peopleSalary)
 
 let ar = [];
 
